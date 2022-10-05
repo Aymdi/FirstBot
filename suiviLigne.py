41b3de313dbc9a0dde19fbe189bbf7216a26c57b
inbouchet <83234731+inbouchet@users.noreply.github.com>
2022-10-05 14:33:32 +0200

    Update suiviLigne.py

--- suiviLigne.py
+++ suiviLigne.py
@@ -1,28 +1,52 @@
+from turtle import pos
 import cv2
 import numpy as np
 
+RED_START = np.array([0,100,100])
+RED_END = np.array([10,255,255])
+
+YELLOW_START = np.array([20,100,100])
+YELLOW_END = np.array([30,255,255])
+
+ORANGE_START = np.array([10,100,20])
+ORANGE_END = np.array([25,255,255])
+
+BLACK_START = np.array([0,0,0])
+BLACK_END = np.array([180,255,30])
+
+A = 0
+B = 0
+
 cam = cv2.VideoCapture(0)
-xvid = cv2.VideoWriter_fourcc(*'XVID')
-video= cv2.VideoWriter('captured_video.avi', xvid, 10, (500,300))
 
 while(True):
     _, img = cam.read()
     
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    
-    red_start = np.array([0,100,100])
-    red_end = np.array([10,255,255])
+    mask = cv2.inRange(hsv, RED_START , RED_END)
 
-    mask = cv2.inRange(hsv, red_start, red_end)
-    new_video = cv2.bitwise_and(img, img, mask= mask)
-    video.write(img)
+    # ligne de test allant de mask[0,0] à mask[img.shape[0],0]
+    for y1 in range (0,img.shape[0]):
+      if(mask[0,y1] == 255):
+        A = y1
+        break
+
+    for y2 in range (img.shape[0],0, -1):
+      if(mask[0,y2] == 255):
+        B = y2
+        break
+
+    position_x = 0
+    position_y =(A + B)/2
+
+    print(position_x, position_y)
   
-    cv2.imshow('new_video',new_video)
+  
+    cv2.imshow('new_video',mask)
 
     if cv2.waitKey(1) & 0xFF == ord('C'):
       break
 
 cam.release()
-video.release()
 
 cv2.destroyAllWindows() 
