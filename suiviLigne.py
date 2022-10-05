7e4cc38a96f580c6fc22955425852d05cb4acb84
inbouchet <83234731+inbouchet@users.noreply.github.com>
2022-10-05 14:39:17 +0200

    Update suiviLigne.py

--- suiviLigne.py
+++ suiviLigne.py
@@ -17,36 +17,39 @@ BLACK_END = np.array([180,255,30])
 A = 0
 B = 0
 
-cam = cv2.VideoCapture(0)
+def ligne(color_start, color_end):
+  cam = cv2.VideoCapture(0)
 
-while(True):
-    _, img = cam.read()
+  while(True):
+      _, img = cam.read()
+      
+      hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+      mask = cv2.inRange(hsv, color_start , color_end)
+
+      # ligne de test allant de mask[0,0] à mask[img.shape[0],0]
+      for y1 in range (0,img.shape[0]):
+        if(mask[0,y1] == 255):
+          A = y1
+          break
+
+      for y2 in range (img.shape[0],0, -1):
+        if(mask[0,y2] == 255):
+          B = y2
+          break
+
+      position_x = 0
+      position_y =(A + B)/2
+
+      print(position_x, position_y)
     
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, RED_START , RED_END)
+    
+      cv2.imshow('new_video',mask)
 
-    # ligne de test allant de mask[0,0] à mask[img.shape[0],0]
-    for y1 in range (0,img.shape[0]):
-      if(mask[0,y1] == 255):
-        A = y1
+      if cv2.waitKey(1) & 0xFF == ord('C'):
         break
 
-    for y2 in range (img.shape[0],0, -1):
-      if(mask[0,y2] == 255):
-        B = y2
-        break
+  cam.release()
 
-    position_x = 0
-    position_y =(A + B)/2
+  cv2.destroyAllWindows() 
 
-    print(position_x, position_y)
-  
-  
-    cv2.imshow('new_video',mask)
-
-    if cv2.waitKey(1) & 0xFF == ord('C'):
-      break
-
-cam.release()
-
-cv2.destroyAllWindows() 
+#toto = ligne(RED_START,RED_END)
