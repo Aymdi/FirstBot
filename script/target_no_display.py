8f8bc6f062d4f4ec56ffeddaeb87260fc7f2bcfe
Ahmed-Manaf DAHMANI <ahmed-manaf.dahmani@bordeaux-inp.fr>
2022-10-07 08:35:12 +0200

    Saving path to txt file.

--- script/target_no_display.py
+++ script/target_no_display.py
@@ -13,16 +13,31 @@ if __name__=="__main__":
     target_y = float(input("Target Y coordinate :"))
 
     posa_a, pos_b = m.get_position()
-    L=[]
-    L.append(P)
-    try:
-        while(True):
-            pos_a_new, pos_b_new = m.get_position()
-            P = next_position(P, R * angle_diff(pos_a_new, posa_a), -R * angle_diff(pos_b_new, pos_b))
-            posa_a = pos_a_new
-            pos_b = pos_b_new
 
-            L.append(P)
+    with open("target.txt", 'w') as fp:
+        for j in range(len(P)):
+            fp.write(str(P[j]))
+            if(j < len(P) - 1):
+                fp.write(" ")
+            else:                   
+                fp.write("\n")
+    
+        try:   
+            while(True):
+                pos_a_new, pos_b_new = m.get_position()
+                P = next_position(P, R * angle_diff(pos_a_new, posa_a), -R * angle_diff(pos_b_new, pos_b))
+                
+                posa_a = pos_a_new
+                pos_b = pos_b_new
+
+                for j in range(len(P)):
+                    fp.write(str(P[j]))
+                    if(j < len(P) - 1):
+                        fp.write(" ")
+                    else:                   
+                        fp.write("\n")
+
+
 
             err_o, err_d = point_to_point(np.array([target_x, target_y]), P)
             #print(P)
@@ -32,14 +47,6 @@ if __name__=="__main__":
                 #print([speed_left, speed_right])
                 m.move(speed_left, speed_right)
 
-    finally:
-        m.stop()
-        m.unclock()
-        with open("target.txt", 'w') as fp:
-            for i in range(len(L)):
-                for j in range(len(L[i])):
-                    fp.write(str(L[i][j]))
-                    if(j < len(L[i]) - 1):
-                        fp.write(" ")
-                if(i < len(L) - 1):
-                    fp.write("\n")
+        finally:
+            m.stop()
+            m.unclock()
