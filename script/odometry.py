83b0879db006169bcb33d97772a4dcfdf222aefb
Ahmed-Manaf DAHMANI <ahmed-manaf.dahmani@bordeaux-inp.fr>
2022-10-07 08:43:04 +0200

    Saving path to txt file.

--- script/odometry.py
+++ script/odometry.py
@@ -9,25 +9,28 @@ if __name__=="__main__":
     plotter = Plotter(1000, 1000, 6)
     m = motor.motor()
     posa_a, pos_b = m.get_position()
-    L=[]
-    L.append(P)
-    try:
-        while(True):
-            pos_a_new, pos_b_new = m.get_position()
+    with open("odometry.txt", 'w') as fp:
+        for j in range(len(P)):
+            fp.write(str(P[j]))
+            if(j < len(P) - 1):
+                fp.write(" ")
+            else:                   
+                fp.write("\n")
+        try:
+            while(True):
+                pos_a_new, pos_b_new = m.get_position()
 
-            plotter.plot2(P[0], P[1])
-            P = next_position(P, R * angle_diff(pos_a_new, posa_a), -R * angle_diff(pos_b_new, pos_b))
-            posa_a = pos_a_new
-            pos_b = pos_b_new
+                plotter.plot2(P[0], P[1])
+                P = next_position(P, R * angle_diff(pos_a_new, posa_a), -R * angle_diff(pos_b_new, pos_b))
+                posa_a = pos_a_new
+                pos_b = pos_b_new
         
-    finally:
-        m.stop()
-        m.unclock()
-        with open("odometry.txt", 'w') as fp:
-            for i in range(len(L)):
-                for j in range(len(L[i])):
-                    fp.write(str(L[i][j]))
-                    if(j < len(L[i]) - 1):
+                for j in range(len(P)):
+                    fp.write(str(P[j]))
+                    if(j < len(P) - 1):
                         fp.write(" ")
-                if(i < len(L) - 1):
-                    fp.write("\n")
+                    else:                   
+                        fp.write("\n")
+        finally:
+            m.stop()
+            m.unclock()
